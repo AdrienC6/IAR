9fec92f2284fb2b3a391b917de61b4731c3c94e2
AdrienC6 <72017587+AdrienC6@users.noreply.github.com>
2021-02-15 23:51:37 +0100

    article show page, login page customization, lot of styling

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -157,4 +157,87 @@ class HomeController extends AbstractController
             'searchResult' => $searchResult
         ]);
     }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function articleShow(ArticleRepository $articleRepository, $id, Request $request, TagRepository $tagRepository, CategoryRepository $categoryRepository): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+        $article = $articleRepository->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException("L'article demandé n'existe pas..."); // Si pas trouvé
+        }
+
+        $tagsArticleAll = $article->getTag();
+        $array = $tagsArticleAll->toArray();
+        $tagsArticle = array_reverse($array);
+
+        $categoriesArticleAll = $article->getCategory();
+        $array2 = $categoriesArticleAll->toArray();
+        $categoriesArticle = array_reverse($array2);
+
+        $articles = $articleRepository->findAll();
+
+        $tagIndex = $tagRepository->findAll();
+
+        $form = $this->createForm(SearchContentType::class);
+        $form->handleRequest($request);
+
+        $searchResult = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('words')->getData() != null) { // Si champ pas vide
+
+                if ($form->get('category')->getData() != null) {
+
+                    if ($form->get('tag')->getData() != null) {
+                        $articlesTag = array_reverse($tagRepository->findOneBy(['name' => $form->get('tag')->getData()->getName()])->getArticles()->toArray());
+                        $articlesCategory = array_reverse($categoryRepository->findOneBy(['name' => $form->get('category')->getData()->getName()])->getArticles()->toArray());
+
+                        foreach ($articlesCategory as $a) {
+                            foreach ($articleRepository->search($form->get('words')->getData()) as $a2) {
+                                foreach ($articlesTag as $a4) {
+                                    $a5 = $a4->getTitle();
+                                    $a3 = $a2->getTitle();
+                                    if ($a3 == $a->getTitle() && $a->getTitle() == $a5) {
+                                        $searchResult[] = $a;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        // On retrouve tous les articles liés à la category
+                        $articlesCategory = array_reverse($categoryRepository->findOneBy(['name' => $form->get('category')->getData()->getName()])->getArticles()->toArray());
+
+                        // On les ajoute tous au résultat
+                        foreach ($articlesCategory as $a) {
+                            foreach ($articleRepository->search($form->get('words')->getData()) as $a2) {
+                                $a3 = $a2->getTitle();
+                                if ($a3 == $a->getTitle()) {
+                                    $searchResult[] = $a;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    $searchResult = $articleRepository->search($form->get('words')->getData());
+                }
+            }
+        }
+
+        return $this->render('home/article_show.html.twig', [
+            'article' => $article,
+            'tags' => $tagsArticle,
+            'categories' => $categoriesArticle,
+            'searchResult' => $searchResult,
+            'form' => $form->createView(),
+            'articles' => $articles,
+            'tagIndex' => $tagIndex
+        ]);
+    }
 }
