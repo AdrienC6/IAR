a72c2b82b5acdc2328203dd341e5c58ca78b7877
AdrienC6 <72017587+AdrienC6@users.noreply.github.com>
2021-02-25 22:39:51 +0100

    add reset password function (maker bundle) + templating of 404,500 & 403 errors

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'label'=>' ',
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez saisir un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                            'max' => 255,
+                        ]),
+                    ],
+                    'label' => ' ',
+                    'attr'=>[
+                        'placeholder'=>'Nouveau mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => ' ',
+                    'attr'=>[
+                        'placeholder'=>'Confirmation'
+                    ]
+                ],
+                'invalid_message' => 'Les deux mots de passe ne sont pas identiques.',
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
